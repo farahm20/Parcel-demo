9d26f0efa70023f29f44e6ea84a603a3bff28a6a
farahm20 <mahboob.farah@gmail.com>
2021-02-08 23:05:51 +0100

    1st commit.

---
+++ src/components/App.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div>
+    <button @click="amount++">Click me!</button>
+    <br/>
+    The button has been clicked  {{amount}} times.
+  </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        amount: 0
+    })
+
+}
+</script>
+
+<style>
+button{
+    padding: 1em;
+    border-radius: 1em;
+}
+
+</style>
